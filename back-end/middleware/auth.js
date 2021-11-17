0a86ac20371aaf2573cc3766bcd7dbc5e5a1ec0e
olga <olga.lescuyer@gmail.com>
2021-11-17 13:38:58 +0100

    add: auth jwt middlaware;

---
+++ back-end/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+// authentification
+const dotenv = require("dotenv");
+// masque
+dotenv.config();
+
+const tokenSecret = process.env.TOKEN;
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, tokenSecret);
+        const u_id = decodedToken.u_id;
+        if (req.body.u_id && req.body.u_id !== u_id) {
+            throw 'User ID non valable';
+        } else {
+            next();
+        }
+
+    } catch (error) {
+        res.status(401).json({ error: error | 'Requête non authentifiée !' });
+    }
+}
