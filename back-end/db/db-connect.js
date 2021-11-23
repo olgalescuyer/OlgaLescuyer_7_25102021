7622099cc84bfc91b52a4cb9237bbf8e494001ef
olga <olga.lescuyer@gmail.com>
2021-11-23 16:41:04 +0100

    fix: add module.export for db;

--- back-end/db/db-connect.js
+++ back-end/db/db-connect.js
@@ -1,10 +1,36 @@
 const mysql = require('mysql');
 const dbConfig = require('./db-config');
 
-const db = mysql.createConnection(dbConfig.connection);
+const db = mysql.createConnection(dbConfig);
 
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Database connected....');
+// db.connect();
 
+// // testing of the connection : 
+// db.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
+
+//     if (error) {
+//         return console.error('error: ' + error.message);
+//     }
+//     // if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+//     console.log('Database connected....');
+// });
+
+// db.end();
+
+db.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + db.threadId);
 });
+
+// db.connect((err) => {
+//     if (err) throw err;
+//     console.log('Database connected....');
+
+// });
+
+module.exports = db;
