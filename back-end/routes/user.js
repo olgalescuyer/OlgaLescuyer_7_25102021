bebd8b9910db158f3711761d203138ebf5cc1f71
olga <olga.lescuyer@gmail.com>
2021-12-06 12:49:18 +0100

    add: loginValidator, profileValidator;

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { signupValidator, loginValidator } = require('../middleware/validator');
+const { signupValidator, loginValidator, profileValidator } = require('../middleware/validator');
 
 const { signup, login, getOneUser, modifyOneUser, deleteOneUser } = require('../controllers/user');
 const auth = require('../middleware/auth');
@@ -10,7 +10,7 @@ router.post('/signup', signupValidator, signup); // new user sign up
 router.post('/login', loginValidator, login); // new user login
 
 router.get('/:id', auth, getOneUser); // get user profile
-router.put('/:id', auth, signupValidator, modifyOneUser); // modify user profile
+router.put('/:id', auth, profileValidator, modifyOneUser); // modify user profile
 router.delete('/:id', auth, deleteOneUser); // delete user profile
 
 module.exports = router;
