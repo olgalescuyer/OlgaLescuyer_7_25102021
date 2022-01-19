937c1d62f3708351ecd11e911ce4a715682960b6
olga <olga.lescuyer@gmail.com>
2022-01-19 17:01:01 +0100

    add: manage of the Likes;

--- front-end/src/Components/Home/Card/Likes.jsx
+++ front-end/src/Components/Home/Card/Likes.jsx
@@ -1,36 +1,62 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
+import React, { useState } from "react";
+
 import { HiThumbUp } from "react-icons/hi";
 import { HiThumbDown } from "react-icons/hi";
 import { HiOutlineThumbUp } from "react-icons/hi";
 import { HiOutlineThumbDown } from "react-icons/hi";
 
-const Likes = ({likes, likeId, likeUserId}) => {
-// console.log(likes, likeId, likeUserId);
-  
- 
+const Likes = ({ likes, likeId, likeUserId }) => {
+  // console.log(likes, likeId, likeUserId);
+
+  const [like, setLike] = useState(0);
+  const handleLike = () => {
+    if (like === 0 && dislike === 0) {
+      setLike(1);
+    } else if (like === 1) {
+      setLike(0);
+    }
+  };
+
+  const [dislike, setDislike] = useState(0);
+  const handleDislike = () => {
+    if (dislike === 0 && like === 0) {
+      setDislike(-1);
+    } else if (dislike === -1) {
+      setDislike(0);
+    }
+  };
+
   return (
     <div className="d-flex ">
-      <div className="position-relative">
-        <span className="position-absolute invisible">
+      <div className="position-relative" onClick={handleLike}>
+        <span
+          className={
+            like === 1 ? "position-absolute" : "position-absolute invisible"
+          }
+        >
           <HiThumbUp size={24} />
         </span>
+
         <span className="">
           <HiOutlineThumbUp size={24} />
         </span>
 
-        <span>{" "+ likes}</span>
+        <span>{like + likes}</span>
       </div>
 
-      <div className="position-relative">
-        <span className="position-absolute invisible">
+      <div className="position-relative" onClick={handleDislike}>
+        <span
+          className={
+            dislike === -1 ? "position-absolute" : "position-absolute invisible"
+          }
+        >
           <HiThumbDown size={24} />
         </span>
         <span className="">
           <HiOutlineThumbDown size={24} />
         </span>
 
-        <span>{" "+ likes}</span>
+        <span>{dislike + likes}</span>
       </div>
     </div>
   );
